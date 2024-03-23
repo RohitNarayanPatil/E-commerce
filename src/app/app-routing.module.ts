f409f75ad2555695bb7de3ec218dcdf25be810a0
Rohit <rnprohitpatil@gmail.com>
2024-03-23 10:56:48 +0530

    working on dashboard module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {path: '', redirectTo:'/accounts/login', pathMatch:'full'},
   { path: 'accounts', loadChildren: () => import('./auth/auth.module').then(mod => mod.AuthModule) },
-  { path: 'dashboard', loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule) },
+  { path: 'home', loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule) },
 ];
 
 @NgModule({
